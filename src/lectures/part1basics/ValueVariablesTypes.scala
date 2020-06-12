230e2059b72683a055891c074fe5b855c507c3ea
Kfurfles <kelvinsilva.ksv@gmail.com>
2020-06-11 21:17:50 -0300

    adição gitignore

--- src/lectures/part1basics/ValueVariablesTypes.scala
+++ src/lectures/part1basics/ValueVariablesTypes.scala
@@ -1,6 +1,39 @@
 package lectures.part1basics
 
 object ValueVariablesTypes extends App{
-  val x = 42;
+  // Integer
+  val x = 42
   println(x)
+
+  // String
+  val aString: String = "Hello"
+  val anotherString: String = "goodbye"
+
+  // Boolean
+  val aBoolean: Boolean = false
+
+
+  // Char
+  val aChar: Char = 'a';
+
+  // Short
+
+  val aShort: Short = 1111
+
+  // Long
+  var aLong: Long = 2134123412342312421L;
+
+  // Float
+  var aFloat: Float = 2.0f;
+
+  // Double
+  var aDouble: Double = 6.0;
+
+  // Variables
+  var aVariable: Int = 4;
+  aVariable = 7;
+
+
+
+
 }
